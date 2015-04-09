0fe37c2db79afef7f76e16e8bab4963a8998fd9b
Elliott Young <elliott.a.young@gmail.com>
2015-04-09 13:38:12 -0500

    Fix DM Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :usergames
   has_many :games, through: :usergames
-  has_many :games, through: :usergames, foreign_key: :dungeon_master
+  has_many :games, foreign_key: :dungeon_master
 end
