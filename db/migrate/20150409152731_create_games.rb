a4a9a35be7ebbd2da017eb20f8448f2068033f28
Elliott Young <elliott.a.young@gmail.com>
2015-04-09 13:25:11 -0500

    Add migration data

--- db/migrate/20150409152731_create_games.rb
+++ db/migrate/20150409152731_create_games.rb
@@ -1,6 +1,15 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
+      t.integer  :dungeon_master
+      t.string   :game_name
+      t.string   :game_system
+      t.string   :level_range
+      t.text     :description
+      t.integer  :minimum_number_of_players
+      t.integer  :maximum_number_of_players
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps null: false
     end
--- db/migrate/20150409152734_create_venues.rb
+++ db/migrate/20150409152734_create_venues.rb
@@ -1,6 +1,17 @@
 class CreateVenues < ActiveRecord::Migration
   def change
     create_table :venues do |t|
+      t.string  :name
+      t.string  :street_address
+      t.string  :street_address_2
+      t.string  :city
+      t.string  :state
+      t.string  :zipcode
+      t.string  :special_instructions
+      t.integer :latitude
+      t.integer :longitude
+      t.time    :opening_time
+      t.time    :closing_time
 
       t.timestamps null: false
     end
