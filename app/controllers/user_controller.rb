f7cfef19cda9f3cdc05d18c3a6bf0c58ed499438
John Schulz <johnschulz75@gmail.com>
2015-04-12 19:44:18 -0500

    Completing user controllers and added 'name' to the Users table

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,26 @@ class UserController < ApplicationController
 		@user = User.new
 	end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path, notice: "Welcome to AirDnD!"
+    else
+      @errors = @user.errors
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+	private
+
+  def user_params
+    params.require(:user).permit(:email, :password_hash, :avatar_url, :bio, :name , :preferred_experience, :preferred_version, :preferred_classes, :preferred_races, :preferred_alignments, :preferred_level)
+  end
+
 end
